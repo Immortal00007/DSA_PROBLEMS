35f1c1f942804a9aa831d944c372a8a9e81d083d
Immortal <dharmahendra007@gmail.com>
2024-01-21 19:19:52 +0530

    Single Number III

---
+++ Single Number III.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        //    int ans =-1;
+           vector<int> ans;
+        map<int,int> m;
+        for(int i =0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for(auto i:m){
+            if(i.second != 2){
+                ans.push_back(i.first);
+            }
+           
+        }
+         return ans ;
+    }
+};
